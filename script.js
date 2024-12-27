b892869ad3be329cfb46fa7ca57eb8539a693806
Omar Nuñez <OmarNnzV@gmail.com>
2024-12-27 14:39:37 -0700

    UPDATE

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+const lblCantidad = $("#lblCantidad");
 const btnContador = $("#btnContador");
+
+fncInit();
 
 function fncInit() {
   fncListeners();
 }
 
 function fncListeners() {
-  btnContador.clic(function () {
-    console.log("entre");
+  fncGetContador();
+  btnContador.click(function () {
+    fncSetContador();
+    fncGetContador();
   });
+}
+
+function fncSetContador() {
+  let contador = localStorage.getItem("contador");
+  localStorage.setItem("contador", (+contador) + 1);
+}
+
+function fncGetContador() {
+  let contador = +localStorage.getItem("contador");
+  lblCantidad.text(`Cantidad: ${contador} `);
 }
